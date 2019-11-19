fd52af67e1008558e17502625a8d6bee8312c4ca
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-11-19 18:12:42 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const inspiration = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fg2reader.com%2Ffeed%2Ffolder%2F5bbcbaecd6498ccd2a047ea63d279b9b%2F%3Ff%3D112549&api_key=po05jsia424nkhv5qmih4dpl13v9g3xsddzjithc&count=100";
-const max = 1000;
+const max = 2000;
 const train = 5;
 var markov = new Markov();
 function generate() {	
