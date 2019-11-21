42a7a567cf6a971ba226311e13aaad8c94ea59d5
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-11-21 12:31:29 +0000

    Using my own inspiration service

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const inspiration = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fg2reader.com%2Ffeed%2Ffolder%2F5bbcbaecd6498ccd2a047ea63d279b9b%2F%3Ff%3D112549&api_key=po05jsia424nkhv5qmih4dpl13v9g3xsddzjithc&count=100";
+const inspiration = "https://aihelper.ndev.tk/";
 const max = 2000;
 const train = 5;
 var markov = new Markov();
@@ -69,7 +69,7 @@ function cleanString(str) { // Input to user
 async function TrainMarkov(markov) {
     let result = await fetch(inspiration)
     json = await result.json();
-    for (const item of json.items) {
+    for (const item of json.rss.channel[0].item) {
         markov.addStates(removeHTML(item.description));
 	markov.addStates(removeHTML(item.title));
     }
